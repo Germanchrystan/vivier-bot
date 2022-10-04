94ff28baf02892f4b18ec6fb9df3ef73c76acfa1
germanchrystan <germanchrystan@gmail.com>
2022-10-04 00:21:34 -0300

    chord tag

--- src/writer/note.go
+++ src/writer/note.go
@@ -6,8 +6,13 @@ import (
 	"github.com/Germanchrystan/vivier-forest/src/composer"
 )
 
-func generateNote(note composer.Note) string {
-	return fmt.Sprintf("<note>\n<pitch>\n<step>%s</step>\n<alter>%f</alter>\n<octave>%d</octave>\n</pitch>\n<duration>1</duration>\n<voice>1</voice><type>quarter</type><accidental>%s</accidental><stem>up</stem></note>",
+func generateNote(note composer.Note, isFirstChordNote bool) string {
+	var chordTag string
+	if isFirstChordNote {
+		chordTag = "<chord/>"
+	}
+	return fmt.Sprintf("<note>%s\n<pitch>\n<step>%s</step>\n<alter>%f</alter>\n<octave>%d</octave>\n</pitch>\n<duration>1</duration>\n<voice>1</voice><type>quarter</type><accidental>%s</accidental><stem>up</stem></note>",
+		chordTag,
 		note.Step,
 		note.Accidental.Alter,
 		note.Octave,
