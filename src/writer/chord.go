5936c74a79bc1d448c3a5f9684af35cc1aa2b421
germanchrystan <germanchrystan@gmail.com>
2022-10-22 16:30:48 -0300

    engrave progression with header

--- src/writer/chord.go
+++ src/writer/chord.go
@@ -2,6 +2,7 @@ package writer
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Germanchrystan/vivier-forest/src/composer"
 )
@@ -16,18 +17,34 @@ func engraveProgression(progression []composer.Chord) (string, string) {
 	measure := 1
 	for index, chord := range progression {
 		var measureSeparator string
+		var measureSeparatorBase, measureSeparatorUpper string
 		if index == 0 {
 			measureSeparator = fmt.Sprintf("<measure number=\"%d\">", measure)
+
+			headerUpper := "<print><system-layout><system-margins><left-margin>0.00</left-margin><right-margin>-0.00</right-margin></system-margins><top-system-distance>170.00</top-system-distance></system-layout></print><attributes><divisions>1</divisions><key><fifths>0</fifths></key><time><beats>4</beats><beat-type>4</beat-type></time><clef><sign>G</sign><line>2</line></clef></attributes>"
+			headerBase := "<print><staff-layout number=\"1\"><staff-distance>65.00</staff-distance></staff-layout></print><attributes><divisions>1</divisions><key><fifths>0</fifths></key><time><beats>4</beats><beat-type>4</beat-type></time><clef><sign>F</sign><line>4</line></clef></attributes>"
+
+			measureSeparatorBase, measureSeparatorUpper = measureSeparator+headerBase, measureSeparator+headerUpper
 		} else if index%4 == 0 {
 			measure++
 			measureSeparator = fmt.Sprintf("</measure>><measure number=\"%d\">", measure)
+			measureSeparatorBase, measureSeparatorUpper = measureSeparator, measureSeparator
 		} else {
-			measureSeparator = ""
+			measureSeparatorBase, measureSeparatorUpper = "", ""
 		}
-		bp, up := engraveChord(chord)
-		basePart += measureSeparator + bp
-		upperPart += measureSeparator + up
 
+		bp, up := engraveChord(chord)
+		basePart += measureSeparatorBase + bp
+		upperPart += measureSeparatorUpper + up
+
+		// End of progression
+		if index == len(progression)-1 {
+			remainingBeatQuantity := 4 - index%4
+			remainingBeats := strings.Repeat("<note><rest/><duration>1</duration><voice>1</voice><type>quarter</type></note>", remainingBeatQuantity)
+
+			basePart += remainingBeats
+			upperPart += remainingBeats
+		}
 	}
 	return basePart, upperPart
 }
